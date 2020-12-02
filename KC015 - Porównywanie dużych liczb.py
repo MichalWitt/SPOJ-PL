9e3317cf84419c50d2caedf9de90f0cab2ff6e24
Michal_Wit <34463513+MichalWitt@users.noreply.github.com>
2020-12-02 16:00:12 +0100

    Add files via upload
    KC015 - Porównywanie dużych liczb SPOJ PL PYTHON

---
+++ KC015 - Porównywanie dużych liczb.py
@@ -0,0 +1,36 @@
+while True:           
+    try:
+        a, b, c = input().split(' ')
+        dluga = (len(a))
+        dlugb = (len(b))
+       
+        if dluga or dlugb <= 2:         
+                       
+            if b == '==':
+                if a == c:
+                    print('1')
+                else:
+                    print('0')
+               
+            elif b == '!=':
+                if a != c:
+                    print('1')
+                else:
+                    print('0')
+               
+            elif b == '>=':
+                a, c = int(a), int(c)
+                
+                if a >= c:
+                    print('1')
+                else:
+                    print('0')
+                   
+            elif b == '<=':
+                a, c = int(a), int(c)
+                if a <= c:
+                    print('1')
+                else:
+                    print('0')         
+    except EOFError:
+        break        
