f975f87e801a2cd386f88085d013f0add877bd5d
Michal_Wit <witczak.gdansk@gmail.com>
2020-12-27 21:18:15 +0100

    Add files via upload
    JZAPKAB - Kabalistyczny zapis daty, SPOJ PL, PYTHON

---
+++ JZAPKAB - Kabalistyczny zapis daty.py
@@ -0,0 +1,7 @@
+a = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'k':10,'l':20,'m':30,'n':40, 'o':50,'p':60,'q':70,'r':80,'s':90,'t':100,'v':200,'x':300,'y':400,'z':500} 
+word = input()
+suma = [] 
+for i in word:
+   if i in a:
+      suma.append(int(a[i]))
+print(sum(suma)) 
